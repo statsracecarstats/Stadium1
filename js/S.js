6e6673b726da7acde19b0933ce3bf804c9622091
Cunningham <pacunningham821@gmail.com>
2018-05-08 23:31:09 -0400

    dots for year vs capacity displayed

--- js/S.js
+++ js/S.js
@@ -9,14 +9,52 @@ var canvas = d3.select(".SCATTER").append("svg")
 				.attr("width", width + margin.left + margin.right)
 				.attr("height", height + margin.top + margin.bottom);
 				
-				
-// set up 				
-var GAH=[];							
-
+//load data set in d3.csv				
 d3.csv("https://raw.githubusercontent.com/pacunningham821/Stadium1/master/Data_Set2_NBA.csv").then(function(data){
-	console.log(data);
-	console.log(data["Capacity"]);
-	var CapMax = d3.max(parseFloat(data.Capacity));
-	console.log(CapMax)
-	GAH = data;
-});							
+	
+	var CapMax = d3.max(data, function(d) {return parseFloat(d.Capacity);});
+	var CapMin = d3.min(data, function(d) {return parseFloat(d.Capacity);});
+	var YeaMax = d3.max(data, function(d) {return parseFloat(d['Year.opened']);});
+	var YeaMin = d3.min(data, function(d) {return parseFloat(d['Year.opened']);});
+	//set scale for x-axis based on year
+	var scaleX = d3.scaleLinear()
+		.domain([YeaMin,YeaMax])
+		.range([50,700]);
+	//set scale for y-axis based on capacity
+	var scaleY = d3.scaleLinear()
+		.domain([CapMin,CapMax])
+		.range([700, 50]);
+	//create scatter points using scale. year vs capacity	
+	canvas.selectAll("circle")
+		.data(data)
+		.enter()
+		.append("circle")
+		.attr("cx", function(d) {return scaleX(parseFloat(d['Year.opened']));})
+		.attr("cy", function(d) {return scaleY(parseFloat(d.Capacity));})
+		.attr("r", 15)
+		.attr("id", function(d) {return d.Stadium;})
+		.on("mouseover", handleMouseOver)
+		.on("mouseout", handleMouseOut)
+		
+	
+	
+	
+	console.log(CapMax);
+	console.log(YeaMin);
+
+});		
+
+function handleMouseOver(d) {
+		var X = parseInt(d3.select(this).attr("cx"));
+		var Y = parseInt(d3.select(this).attr("cy"));
+		
+		canvas.append("text")
+			.attr("x", X + 7)
+			.attr("y", Y - 7)
+			.attr("id", "TXT0")
+			.text("(" + d['Year.opened'] + ", " + d.Capacity + ")");
+}
+
+function handleMouseOut(d) {
+	d3.select("#TXT0").remove();
+}					
